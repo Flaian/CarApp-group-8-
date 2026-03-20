69770b1773433a25e1e87a03a8fafddca2b6eb21
Andreas Hemmer <andreas.hemmer@hotmail.com>
2026-03-20 15:53:52 +0100

    Refactor Trip class: public, properties, methods added
    Converted Trip to a public class with explicit properties and private setters. Added constructor for initialization and methods for duration calculation and formatted trip details. Cleaned up namespace and removed unused using statements.

--- ConsoleApp4/Trip.cs
+++ ConsoleApp4/Trip.cs
@@ -1,10 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CarAppGroup8
+﻿namespace CarAppGroup8
 {
-    internal class Trip
+    public class Trip
     {
+
+        // Private felt – Car-referencen må ikke ændres udefra
+
+        private readonly Car _car;
+
+
+        public double Distance { get; private set; }
+        public DateTime TripDate { get; private set; }
+        public DateTime StartTime { get; private set; }
+        public DateTime EndTime { get; private set; }
+
+
+        // Public property så Drive() i Car kan sammenligne med 'this'
+        public Car Car => _car;
+
+
+        public Trip(Car car, double distance, DateTime startTime, DateTime endTime)
+        {
+            _car = car;
+            Distance = distance;
+            StartTime = startTime;
+            EndTime = endTime;
+            TripDate = startTime.Date; // Udledes automatisk
+        }
+
+
+        // Returnerer turens varighed
+        public TimeSpan CalculateDuration()
+        {
+            return EndTime - StartTime;
+        }
+
+
+        // Returnerer turens data som formateret tekst
+        public string GetTripDetails()
+        {
+            return $"Dato: {TripDate:dd-MM-yyyy} | " +
+                   $"Distance: {Distance} km | " +
+                   $"Start: {StartTime:HH:mm} | " +
+                   $"Slut: {EndTime:HH:mm} | " +
+                   $"Varighed: {CalculateDuration()}";
+        }
     }
 }
