2d0ad9df33da7cabbb00ef46d6d043b1e8853371
Arunakun <krho79123@edu.ucl.dk>
2026-03-22 07:33:07 -0700

    Trip.cs constructor, fields and properties updated

--- ConsoleApp4/Trip.cs
+++ ConsoleApp4/Trip.cs
@@ -1,8 +1,78 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 
-public class Class1
+public class Trip
 {
-	public Class1()
+    private Car _car;
+    private double _distance;
+	private DateTime _startTime;
+	private DateTime _endTime;
+    private DateTime _tripDate;
+
+    public Trip(Car car, double distance, DateTime startTime, DateTime endTime)
+    {
+        _car = car;
+        _distance = distance;
+		_startTime = startTime;
+		_endTime = endTime;
+        _tripDate = startTime.Date;
+    }
+
+	// Read-only property
+	public Car Car
 	{
+		get
+		{
+            return _car;
+		}
 	}
+
+    // Read+Write properties
+    public double Distance
+    {
+        get
+        {
+            return _distance;
+        }
+        private set
+        {
+            _distance = value;
+        }
+    }
+
+    public DateTime StartTime
+    {
+        get
+        {
+            return _startTime;
+        }
+        private set
+        {
+            _startTime = value;
+        }
+    }
+
+    public DateTime EndTime
+    {
+        get
+        {
+            return _endTime;
+        }
+        private set
+        {
+            _endTime = value;
+        }
+    }
+
+    public DateTime TripDate
+    {
+        get
+        {
+            return _tripDate;
+        }
+        private set
+        {
+            _tripDate = value;
+        }
+    }
 }
