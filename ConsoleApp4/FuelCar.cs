e7508c00925fcf82f34b1fbd0a3070a61f7dfd8d
Arunakun <krho79123@edu.ucl.dk>
2026-03-25 16:43:33 -0700

    Added FuelCar.cs

---
+++ ConsoleApp4/FuelCar.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class FuelCar : Car
+{
+	public double tankCapacity;
+	public double fuelLevel;
+	public double kmPrLiter;
+
+	// Constructor
+	public FuelCar(string brand, string model, int year, string licensePlate, double tankcapacity, double fuellevel, double kmprliter)
+		: base(brand, model, year, licensePlate)
+	{
+        tankCapacity = tankcapacity;
+        fuelLevel = fuellevel;
+        kmPrLiter = kmprliter;
+	}
+
+	// Simple properties
+	public double TankCapacity
+	{
+		get;
+		private set;
+	}
+
+	public double FuelLevel
+	{
+		get;
+		private set;
+	}
+
+	public double KmPrLiter
+	{
+		get;
+		private set;
+	}
+
+	// Override of UpdateEnergyLevel()
+	public override void UpdateEnergyLevel(double km)
+	{
+		FuelLevel -= km / KmPrLiter;
+		if (FuelLevel < 0)
+		{
+			FuelLevel = 0;
+		}
+	}
+
+	// Method - Refuel()
+	public void Refuel(double liters)
+	{
+		FuelLevel = Math.Min(FuelLevel + liters, TankCapacity);
+	}
+}
