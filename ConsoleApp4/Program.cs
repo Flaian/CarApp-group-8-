fc19192db3463dbd6eef0e4d2ec6c6383e806c25
Arunakun <krho79123@edu.ucl.dk>
2026-03-25 17:30:55 -0700

    Program.cs changed to accomodate opgavesæt 7 exercise 4.2

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -4,39 +4,19 @@
     {
         static void Main(string[] args)
         {
-            double fuelPrice = 14.33;
-            double tripPrice = 0;
-            bool palindrome = false;
+            List<Car> cars = new List<Car>();
+            cars.Add(new FuelCar("Hyundai", "i10", 2012, "EC36063", 35.0, 20.0));
+            cars.Add(new ElectricCar("Tesla", "Model 3", 2022, "EL99999", 75.0, 6.5));
+            cars.Add(new FuelCar("Hyundai", "i10", 2022, "CU26620", 35.0, 24.0));
 
-            FuelCar fuelCar = new FuelCar("Hyundai", "i10", 2012, "EC36063", 50.0, 18.0);
-            fuelCar.ToggleEngine();
-
-            ElectricCar electricCar = new ElectricCar("Tesla", "Model 3", 2022, "EL99999", 75.0, 6.5);
-            electricCar.ToggleEngine();
-
-            Trip trip1 = new Trip(fuelCar, 80, DateTime.Now, DateTime.Now.AddHours(1));
-            Trip trip2 = new Trip(electricCar, 60, DateTime.Now, DateTime.Now.AddHours(1));
-
-            fuelCar.Drive(trip1);
-            electricCar.Drive(trip2);
-
-            Console.WriteLine($"FuelCar odometer: {fuelCar.Odometer} km");
-            Console.WriteLine($"Fuel level: {fuelCar.FuelLevel:F1} L");
-
-            Console.WriteLine($"ElectricCar odometer: {electricCar.Odometer} km");
-            Console.WriteLine($"Battery level: {electricCar.BatteryLevel:F1} kWh");
-
-            foreach (Trip trip in fuelCar.GetTrips())
+            foreach (Car car in cars)
             {
-                Console.Write(trip.GetTripDetails());
+                car.ToggleEngine();
+                Trip trip = new Trip(car, 60, DateTime.Now, DateTime.Now.AddHours(1));
+                car.Drive(trip);
+                Console.WriteLine($"{car.Brand} odometer: {car.Odometer} km");
+                car.ToggleEngine();
             }
-
-            List <Trip> tripsByDate = fuelCar.GetTripsByDate(DateTime.Today);
-
-            DateTime start = DateTime.Today;
-            DateTime end = DateTime.Today.AddHours(1).AddMinutes(30);
-
-            List<Trip> tripsInTimeInterval = fuelCar.GetTripsInTimeInterval(start, end);
         }
     }
 }
