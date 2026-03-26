7fb041f3dff63ea45828dfb9e1b3d5a6a5d979c6
Arunakun <krho79123@edu.ucl.dk>
2026-03-25 18:42:32 -0700

    Added a House class

---
+++ ConsoleApp4/House.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class House : ISellable, IInsurable
+{
+    public string Address { get; }
+    public int YearBuilt { get; }
+    public double Price { get; }
+    public string RegistrationNumber { get; } // Cadastral number
+
+    // Constructor
+    public House(string address, int yearBuilt, double price, string cadastralNumber)
+    {
+        Address = address;
+        YearBuilt = yearBuilt;
+        Price = price;
+        RegistrationNumber = cadastralNumber;
+    }
+
+    public string GetSalesSummary()
+    {
+        return $"{Address}, built in {YearBuilt}, price: {Price:N0} DKK";
+    }
+
+    public double GetInsuranceRate()
+    {
+        return YearBuilt < 1980 ? 1.8 : 1.2; // Older houses are more expensive to insure
+    }
+}
