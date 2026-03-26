7fad6e404e2b4607e2ccf6e1d3361877af35b270
Arunakun <krho79123@edu.ucl.dk>
2026-03-25 17:52:57 -0700

    Updated FuelCar.cs

--- ConsoleApp4/FuelCar.cs
+++ ConsoleApp4/FuelCar.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 
-public class FuelCar : Car
+public class FuelCar : Car, ISellable, IInsurable
 {
 	public double TankCapacity { get; private set; }
     public double FuelLevel { get; private set; }
     public double KmPrLiter { get; private set; }
+	public double Price => CalculatePrice();
 
     // Constructor
     public FuelCar(string brand, string model, int year, string licensePlate, double tankCapacity, double kmPrLiter)
@@ -29,5 +30,19 @@ public class FuelCar : Car
 	public void Refuel(double liters)
 	{
 		FuelLevel = Math.Min(FuelLevel + liters, TankCapacity);
+	}
+
+	// Method - CalculatePrice()
+	public double CalculatePrice()
+	{
+		// Just a simplistic example
+		return 100000 - (Odometer * 0.5);
+	}
+
+	// Method - GetInsuranceRate()
+	public double GetInsuranceRate()
+	{
+		// Simplistic example
+		return 5000 + (Odometer * 0.02);
 	}
 }
