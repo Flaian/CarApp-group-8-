ad11d76c42a0be622bfb599143772b99b79f71fa
Arunakun <krho79123@edu.ucl.dk>
2026-03-25 17:54:32 -0700

    Updated ElectricCars.cs

--- ConsoleApp4/ElectricCar.cs
+++ ConsoleApp4/ElectricCar.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 
-public class ElectricCar : Car
+public class ElectricCar : Car, ISellable, IInsurable
 {
 	public double BatteryCapacity { get; private set; }
 	public double BatteryLevel { get; private set; }
     public double KmPrKwh { get; private set; }
+	public double Price => CalculatePrice();
 
     // Constructor
     public ElectricCar(string brand, string model, int year, string licensePlate, double batteryCapacity, double kmPrKwh)
@@ -30,4 +31,18 @@ public class ElectricCar : Car
 	{
 		BatteryLevel = Math.Min(BatteryLevel + kwh, BatteryCapacity);
 	}
+
+    // Method - CalculatePrice()
+    public double CalculatePrice()
+    {
+        // Just a simplistic example - Electric cars keep value better than fuel cars
+        return 150000 - (Odometer * 0.3);
+    }
+
+    // Method - GetInsuranceRate()
+    public double GetInsuranceRate()
+    {
+        // Simplistic example - different risk profile
+        return 4000 + (Odometer * 0.015);
+    }
 }
