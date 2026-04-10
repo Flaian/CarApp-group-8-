e82dc9b2b51e11c17ee001293053e016fb4637db
Arunakun <krho79123@edu.ucl.dk>
2026-04-10 03:24:26 -0700

    Refactor car loading and add duplicate license debug output
    Removed old sample car creation code in Program.cs and now load cars from file using DataHandler. Added console output to indicate when a duplicate license plate is generated. Updated cars.txt with a new ElectricCar entry and included a duplicate license plate for testing.

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -4,20 +4,6 @@
     {
         static void Main(string[] args)
         {
-            /*FuelCar fuelCar = new FuelCar("Toyota", "Corolla", 2022, "AB12345", 50, 18);
-            ElectricCar electricCar = new ElectricCar("Tesla", "Model 3", 2023, "CD67890", 75, 6.5);
-            FuelCar fuelCarBimmer = new FuelCar("BMW", "320d", 2021, "XY99999", 60, 15);
-
-            List<Car> cars = new List<Car>();
-            cars.Add(fuelCar);
-            cars.Add(electricCar);
-            cars.Add(fuelCarBimmer);
-
-            DataHandler dh = new DataHandler("..\\..\\..\\cars.txt");
-            dh.SaveCarsToFile(cars);
-            dh.LoadCarsFromFile();*/
-
-
             DataHandler dh = new DataHandler("..\\..\\..\\cars.txt");
 
             // Load cars from file
@@ -99,6 +85,7 @@
                     if (car.LicensePlate == licensePlate)
                     {
                         isDuplicate = true;
+                        Console.WriteLine("Duplicate license plate generated: " + licensePlate);
                         break; // Skip duplicate license plate
                     }
                 }
