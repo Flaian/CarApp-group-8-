3ba86bf3bbc4bceb19e09f41ad1a51e6e0612b1d
Arunakun <krho79123@edu.ucl.dk>
2026-05-27 18:57:21 +0200

    Add WPF project and initial MVVM structure
    Added a new WPF project `CarApp.Wpf` to the solution with
    basic configurations and references. Created `App.xaml`
    and `App.xaml.cs` for application entry. Added `CarView.xaml`
    and `CarView.xaml.cs` as the main window. Implemented
    `RelayCommand` for MVVM command binding. Included an
    empty `CarViewModel` for future development.

---
+++ CarApp.Wpf/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CarApp.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ CarApp.Wpf/ViewModels/CarViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using CarApp.Core.Models;
+using CarApp.Core.Repositories;
+
+namespace CarApp.Wpf.ViewModels
+{
+    internal class CarViewModel
+    {
+    }
+}
