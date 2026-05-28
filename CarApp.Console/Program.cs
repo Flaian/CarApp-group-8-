c19f1e33d81635d0a91706f9ac5c040f612e6986
Arunakun <krho79123@edu.ucl.dk>
2026-05-28 11:48:44 +0200

    no commit message could be generated from this thanks a lot copilot you fucking useless piece of I love you very much but please fucking work when I ask you to fucking work

--- CarApp.Console/Program.cs
+++ CarApp.Console/Program.cs
@@ -14,7 +14,7 @@ namespace CarApp.ConsoleApp
             ICarRepository repo = new FileCarRepository("cars.txt");
 
 
-            File.WriteAllText("cars.txt", string.Empty); // Clear file before testing
+            File.WriteAllText("cars.txt", string.Empty);
 
             // Add cars
             repo.Add(new FuelCar("Toyota", "Corolla", 2022, "AB12345", 50, 18, 45000));
@@ -48,100 +48,6 @@ namespace CarApp.ConsoleApp
             {
                 Console.WriteLine(car);
             }
-
-
-
-            /*DataHandler dh = new DataHandler("..\\..\\..\\cars.txt");
-
-            // Load cars from file
-            List<Car> cars = dh.LoadCarsFromFile();
-
-            // Add random cars
-            cars.Add(CreateRandomCar(cars));
-
-            // Print list
-            Console.WriteLine("Current cars:\n");
-            foreach (Car car in cars)
-            {
-                Console.WriteLine(car.ToString());
-            }
-
-            // Save updated list to file
-            dh.SaveCarsToFile(cars);*/
         }
-
-
-        /*// Method to generate random cars and add them to the list
-        static Car CreateRandomCar(List<Car> cars)
-        {
-            Random rnd = new Random();
-
-            if (rnd.Next(2) == 0)
-            {
-                FuelCar fuelCar = new FuelCar(
-                    GetRandomFuelBrand(rnd), GetRandomFuelModel(rnd), rnd.Next(2000, 2025),
-                    GetRandomPlate(rnd, cars), rnd.Next(40, 80), rnd.Next(10, 25));
-                return fuelCar;
-            }
-
-            else
-            {
-                ElectricCar electricCar = new ElectricCar(
-                    GetRandomElectricBrand(rnd), GetRandomElectricModel(rnd), rnd.Next(2018, 2025),
-                    GetRandomPlate(rnd, cars), rnd.Next(50, 100), rnd.Next(4, 8));
-                return electricCar;
-            }
-        }
-
-        // Helper methods to generate random data
-        static string GetRandomFuelBrand(Random rnd)
-        {
-            string[] brands = { "Toyota", "BMW", "Audi", "Ford", "Volkswagen" };
-            return brands[rnd.Next(brands.Length)];
-        }
-
-        static string GetRandomFuelModel(Random rnd)
-        {
-            string[] models = { "Corolla", "320d", "A4", "Focus", "Golf" };
-            return models[rnd.Next(models.Length)];
-        }
-
-
-        static string GetRandomElectricBrand(Random rnd)
-        {
-            string[] brands = { "Tesla", "Polestar", "Nissan", "Hyundai", "Kia" };
-            return brands[rnd.Next(brands.Length)];
-        }
-
-        static string GetRandomElectricModel(Random rnd)
-        {
-            string[] models = { "Model 3", "Model Y", "Leaf", "Ioniq 5", "EV6" };
-            return models[rnd.Next(models.Length)];
-        }
-
-
-        static string GetRandomPlate(Random rnd, List<Car> cars)
-        {
-            while (true)
-            {
-                string licensePlate = $"{(char)rnd.Next('A', 'Z')}{(char)rnd.Next('A', 'Z')}{rnd.Next(10000, 99999)}";
-
-                bool isDuplicate = false;
-                foreach (Car car in cars)
-                {
-                    if (car.LicensePlate == licensePlate)
-                    {
-                        isDuplicate = true;
-                        Console.WriteLine("Duplicate license plate generated: " + licensePlate);
-                        break; // Skip duplicate license plate
-                    }
-                }
-
-                if (isDuplicate == false)
-                {
-                    return licensePlate;
-                }
-            }
-        }*/
     }
 }
